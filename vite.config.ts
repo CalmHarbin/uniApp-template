511b20cef7b197930240cc9a8bd2f0fa6a362614
zhanghuan <363639121@qq.com>
2022-03-05 23:12:03 +0800

    :wrench: 新增环境变量配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    envDir: resolve(__dirname, 'config'),
     plugins: [uni()],
     resolve: {
         // 配置别名
